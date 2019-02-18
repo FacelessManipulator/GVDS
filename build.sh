31a39717f00a8cebeee153da9927daad8665c5f9
Hanjie Zhou <miller_maxwell@outlook.com>
2019-02-18 10:24:45 +0800

    Basic project structure
    Signed-off-by: Hanjie Zhou <miller_maxwell@outlook.com>

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git submodule init
+git submodule update
+cmake -H. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=ON
+cd build/ && make
+
---
+++ src/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "hvsd started" << endl;
+    return 0;
+}

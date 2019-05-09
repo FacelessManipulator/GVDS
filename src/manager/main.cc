8423e56b8275216b21d63353cddf5ba049b7185a
Hanjie Zhou <miller_maxwell@outlook.com>
2019-05-09 15:06:36 +0800

    add manager exe
    Signed-off-by: Hanjie Zhou <miller_maxwell@outlook.com>

---
+++ src/manager/main.cc
@@ -0,0 +1,23 @@
+#include <csignal>
+#include "context.h"
+#include "manager/manager.h"
+
+using namespace hvs;
+using namespace std;
+
+void sig_exit(int sig) {
+  dout(-1) << "receive signal " << sig << ", exiting..." << dendl;
+  hvs::destroy_manager(static_cast<Manager*>(HvsContext::get_context()->node));
+  hvs::destroy_context();
+  exit(1);
+}
+
+int main() {
+  init_context();
+  hvs::Manager* mgr = init_manager();
+  sleep(1);
+  dout(-1) << "manager server frontend started at " << mgr->rest_port() << dendl;
+  std::signal(SIGINT, sig_exit);
+  std::signal(SIGTERM, sig_exit);
+  mgr->join();
+}

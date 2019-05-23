46424f88d64cab90b2f9a685c839fa0165baa0a6
franksyao <songyao1@126.com>
2019-05-23 07:02:31 +0000

    temperary test
    Signed-off-by: franksyao <songyao1@126.com>

--- tests/zone/test_zone.cc
+++ tests/zone/test_zone.cc
@@ -67,46 +67,20 @@ class HVSZoneTest : public ::testing::Test {
 //     client.shutdown();
 // }
 
-// TEST_F(HVSZoneTest, Locate) {
-//     Http::Client client;
-//     char url[256];
-//     snprintf(url, 256, "http://localhost:%d/zone/locate", manager->rest_port());
-//     auto opts = Http::Client::options().threads(1).maxConnectionsPerHost(8);
-//     client.init(opts);
-
-//     GetZoneLocateInfoReq req;
-//     req.clientID = "121";
-//     req.zoneID = "7bf24cee-6499-47f1-b9d8-90137255c2a2";
-//     req.spaceID.emplace_back("9700a9cd-d6b5-4aec-9e47-32c22e36352a");
-//     //req.spaceID.emplace_back("1213411-2");
-
-//     std::string value = req.serialize();
-
-//     auto response = client.post(url).body(value).send();
-//         dout(-1) << "Client Info: post request " << url << dendl;
-
-//     std::promise<bool> prom;
-//     auto fu = prom.get_future();
-//     response.then(
-//         [&](Http::Response res) {
-//         dout(-1) << "Manager Info: " << res.body() << dendl;
-//         prom.set_value(true);
-//         },
-//         Async::IgnoreException);
-//     EXPECT_TRUE(fu.get());
-//     client.shutdown();
-// }
-
-TEST_F(HVSZoneTest, GetInfo) {
+TEST_F(HVSZoneTest, Locate) {
     Http::Client client;
     char url[256];
-    snprintf(url, 256, "http://localhost:%d/zone/info", manager->rest_port());
+    snprintf(url, 256, "http://localhost:%d/zone/locate", manager->rest_port());
     auto opts = Http::Client::options().threads(1).maxConnectionsPerHost(8);
     client.init(opts);
 
+    GetZoneLocateInfoReq req;
+    req.clientID = "121";
+    req.zoneID = "7bf24cee-6499-47f1-b9d8-90137255c2a2";
+    req.spaceID.emplace_back("9700a9cd-d6b5-4aec-9e47-32c22e36352a");
+    //req.spaceID.emplace_back("1213411-2");
 
-
-    std::string value = "123";
+    std::string value = req.serialize();
 
     auto response = client.post(url).body(value).send();
         dout(-1) << "Client Info: post request " << url << dendl;
@@ -122,65 +96,34 @@ TEST_F(HVSZoneTest, GetInfo) {
     EXPECT_TRUE(fu.get());
     client.shutdown();
 }
+
+// TEST_F(HVSZoneTest, GetInfo) {
+//     Http::Client client;
+//     char url[256];
+//     snprintf(url, 256, "http://localhost:%d/zone/info", manager->rest_port());
+//     auto opts = Http::Client::options().threads(1).maxConnectionsPerHost(8);
+//     client.init(opts);
+
+
+
+//     std::string value = "124";
+
+//     auto response = client.post(url).body(value).send();
+//         dout(-1) << "Client Info: post request " << url << dendl;
+
+//     std::promise<bool> prom;
+//     auto fu = prom.get_future();
+//     response.then(
+//         [&](Http::Response res) {
+//         dout(-1) << "Manager Info: " << res.body() << dendl;
+//         prom.set_value(true);
+//         },
+//         Async::IgnoreException);
+//     EXPECT_TRUE(fu.get());
+//     client.shutdown();
+// }
 /*
 
-
-TEST_F(HVSZoneTest, GetInfo) {
-    cout<< "******start client: getzoneinfo ******"<<endl;
-
-    // 第二个参数传地址 第三个参数传请求数量 默认1
-    std::string page = "http://localhost:9080/zone/info";//gai
-    int count = 1;
-
-    Http::Client client;
-
-    auto opts = Http::Client::options()
-        .threads(1)
-        .maxConnectionsPerHost(8);
-    client.init(opts);
-
-    std::vector<Async::Promise<Http::Response>> responses;
-
-    std::atomic<size_t> completedRequests(0);
-    std::atomic<size_t> failedRequests(0);
-
-    //计时
-    auto start = std::chrono::steady_clock::now();
-
-    std::string value = "123";
-
-    auto resp = client.post(page).cookie(Http::Cookie("FOO", "bar")).body(value).send();
-    resp.then([&](Http::Response response) {
-            ++completedRequests;
-        std::cout << "Response code = " << response.code() << std::endl;
-        //response body
-        auto body = response.body();
-        if (!body.empty()){
-            std::cout << "Response body = " << body << std::endl;
-            //====================
-            //your code write here
-
-            //====================
-        }
-    }, Async::IgnoreException);
-    responses.push_back(std::move(resp));
-
-    auto sync = Async::whenAll(responses.begin(), responses.end());
-    Async::Barrier<std::vector<Http::Response>> barrier(sync);
-    barrier.wait_for(std::chrono::seconds(5));
-
-    auto end = std::chrono::steady_clock::now();
-    std::cout << "Summary of execution" << std::endl
-              << "Total number of requests sent     : " << count << std::endl
-              << "Total number of responses received: " << completedRequests.load() << std::endl
-              << "Total number of requests failed   : " << failedRequests.load() << std::endl
-              << "Total time of execution           : "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-
-    client.shutdown();
-
-    cout<< "******endl client: getzoneinfo ******"<<endl;
-}
 
 TEST_F(HVSZoneTest, Share) {
     cout<< "******start client: zoneshare ******"<<endl;
@@ -511,7 +454,7 @@ TEST_F(HVSZoneTest, MapDeduct) {
 // }
 
 
-// TEST_F(HVSZoneTest, Info){
+// TEST_F(HVSZoneTest, simpleInfo){
 //     Zone tmp;
 //     std::shared_ptr<hvs::CouchbaseDatastore> zonePtr = std::make_shared<hvs::CouchbaseDatastore>(
 //           hvs::CouchbaseDatastore("zone_info"));
